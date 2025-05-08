d148c12c3bfcbf8c08b75790fb790d2c6beb2bbc
Bree Garey <breegarey@icloud.com>
2025-05-07 20:51:56 -0600

    First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,28 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert "Congratulations!" in browser.title
-print("OK")
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # I heard about a cool new online to-do app. 
+        # so I went to check out its home page
+        self.browser.get("http://localhost:8000")
+
+        #Here is where I notice the page title and header 
+        self.assertIn("To-Do", self.browser.title)
+
+        # I am invited to enter a to-do item straight away
+        self.fail("Finish the test!")
+
+
+#I type in "make some slides"
+
+#When I hit enter, the page updates, and now the page 
+#lists "make some slides" as an item
+if __name__ == "__main__":
+    unittest.main()
