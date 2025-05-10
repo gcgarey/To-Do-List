f5a5a956a92c70bc82012c046301cd32421965e6
Bree Garey <breegarey@icloud.com>
2025-05-09 20:17:04 -0400

    functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -15,14 +18,29 @@ class NewVisitorTest(unittest.TestCase):
 
         #Here is where I notice the page title and header 
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
 
         # I am invited to enter a to-do item straight away
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
+        #I type in "make some slides"
+        inputbox.send_keys("Make some slides")
 
-#I type in "make some slides"
+        #When I hit enter, the page updates, and now the page 
+        #lists "make some slides" as an item
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-#When I hit enter, the page updates, and now the page 
-#lists "make some slides" as an item
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: Make some slides" for row in rows))
+
+        #there is still a text box inviting me to add another item.
+        #enter "read the textbook"
+        self.fail("finish the test")
+
+        #the page updates again and shows both items on my list
 if __name__ == "__main__":
     unittest.main()
